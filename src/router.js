10bacd51b9bb9698fe2455e32667a04fde8e51e2
khizerdev <46632721+khizerdev@users.noreply.github.com>
2023-11-12 21:21:49 +0500

    Group transitions

--- src/router.js
+++ src/router.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "./pages/Home.vue";
+import List from "./pages/List.vue";
 
 const routes = [
   {
@@ -15,6 +16,11 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () =>
       import(/* webpackChunkName: "about" */ "./pages/About.vue"),
+  },
+  {
+    path: "/list",
+    name: "list",
+    component: List,
   },
 ];
 
